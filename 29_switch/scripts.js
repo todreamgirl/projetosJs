7680aa59260d9db33bce303c95bf5cbe14223207
unknown <jacquelinee.barbosa@hotmail.com>
2021-05-06 22:38:30 -0300

    Estruturas de Programacao

---
+++ 29_switch/scripts.js
@@ -0,0 +1,22 @@
+let nome = "Jacqueline";
+
+switch (nome) {
+    case "Jacqueline":
+        console.log("O nome é Jacqueline");
+        break;
+    case "Carlos":
+        console.log("O nome é Carlos");
+        break;
+    default:
+        console.log("O nome não foi encontrado!");
+}
+
+// O if é uma maneira muito mais simples de executar:
+
+// if(nome=="Jacqueline"){
+//     console.log("O nome é Jacqueline");
+// }else if(nome=="Carlos"){
+//     console.log("O nome é Carlos");
+// }else{
+//     console.log("O nome não foi encontrado");
+// }
