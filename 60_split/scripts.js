e1d5f12d0aeb51fbf99171b234dc6777746c5aa6
Jacqueline Alves Barbosa <jacquelinee.barbosa@hotmail.com>
2021-05-21 10:42:04 -0300

    metodosArray&String

---
+++ 60_split/scripts.js
@@ -0,0 +1,14 @@
+let frase = "Testando o método split";
+
+console.log(frase.split(" "));;
+
+let palavras = frase.split(" ");
+
+console.log(palavras);
+
+let produtos = "banana;maçã;jaca;pera;bola;caneca";
+
+console.log(produtos.split(';'));
+
+
+// o metodo split divide a string por um separador e retora um array de palavras
